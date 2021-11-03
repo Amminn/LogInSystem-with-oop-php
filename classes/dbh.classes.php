f3b9b741059a0d1f504df1269feb0b188c59a63c
Amminn <vrb.nnn@gmail.com>
2021-11-03 12:12:21 +0100

    progess

---
+++ classes/dbh.classes.php
@@ -0,0 +1,17 @@
+<?php 
+
+class Dbh {
+
+    private function connect() {
+        try {
+            $userName = "root";
+            $password = "";
+            $dbh = new PDO('mysql:host=localhost;dbname=ooplogin', $userName, $password);
+            return $dbh;
+        } catch (PDOException $e) {
+            print "Error!: " . $e->getMessage() . "<br/>";
+            die(); // kill the connection 
+        }
+    }
+
+}
--- classes/signup.classes.php
+++ classes/signup.classes.php
@@ -1,17 +1,67 @@
 <?php 
 
-class SignupContr{
+class SignupContr {
 
     private $uid;
     private $pwd;
     private $pwdrepeat;
     private $email;
 
-    public function __construct($uid, $pwd, $pwdRepeat, $email) {
-        $this-> uid = $uid;
-        $this-> pwd = $pwd;
-        $this-> pwdrepeat = $pwdrepeat;
-        $this-> email = $email         
+    public function __construct($uid, $pwd, $pwdrepeat, $email) {
+        $this->uid = $uid;
+        $this->pwd = $pwd;
+        $this->pwdrepeat = $pwdrepeat;
+        $this->email = $email;
+    }
+
+    private function emptyInput() {
+        $result;
+        if(empty($this->$uid) || empty($this->$pwd) || empty($this->$pwdrepeat) || empty($this->$email)) {
+            $result = false;
+        }
+        else {
+            $result = true;
+        }
+        return $result;
+    }
+
+    private function invalidUid() {
+        $result;
+        if(!preg_match("/^[a-zA-Z0-9]*$/", $this->Uid))
+        {
+            $result = false;
+        }
+        else
+        {
+            $result = true;
+        }
+        return $result;
+    }
+
+    private function invalidEmail() {
+        $result;
+        if(!filter_var($this->email, FILTER_VALIDATE_EMAIL))
+        {
+            $result = false;
+        }
+        else
+        {
+            $result = true;
+        }
+        return $result;
+    }
+
+    private function pwdMatch() {
+        $result:
+        if($this->pwd !== $this->pwdRepeat)
+        {
+            $result = false;
+        }
+        else
+        {
+            $result = true;
+        }
+        return $result;
     }
 
 }
